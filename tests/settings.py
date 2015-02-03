513d6f99d37ed6159a367fc68bf6aad5c813a116
GermanoGuerrini <germano.guerrini@gmail.com>
2015-02-03 13:06:18 +0100

    Fixes testing under Django 1.7

--- tests/settings.py
+++ tests/settings.py
@@ -17,3 +17,8 @@ SECRET_KEY = 'sk'
 MIDDLEWARE_CLASSES = ()
 
 MAAT_FLUSH_BATCH_SIZE = 100 # To avoid sqlite issues
+
+# This will prevent the tests from running the migrations that come with the app
+# otherwise the test models won't be created.
+# http://stackoverflow.com/questions/25161425/disable-migrations-when-running-unit-tests-in-django-1-7
+MIGRATION_MODULES = {'djangomaat': 'djangomaat.no_migrations_here'}
